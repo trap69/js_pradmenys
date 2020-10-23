17ed9119d9527004930b6e52f3cb65408d318392
jkunciene <jolita.kunciene@kitm.lt>
2020-10-23 15:04:39 +0300

    first commit

---
+++ script/js/app.js
@@ -0,0 +1,48 @@
+
+
+/*var vardas ='Bilas' ;
+var name = "Arnoldas"; //string
+let count = 10; //integer
+let total = 10.50; //float
+let online = true; //boolean
+let items = ['item1', 'item2', 'item3']; //array
+let human = { //object
+    eyes:2,
+    hands:2,
+    name:"x-man"
+};
+
+const appName = "JS Basics tevas dede \"xjonxasx\""; //constant
+
+console.log("KIEKIS: "+ count ); //isvedimas + incrimentavimas vienetu
+console.log(vardas.length);
+console.log(vardas[3]);
+console.log(vardas.indexOf("las"));
+console.log(vardas.slice(0,2));
+*/
+
+/*
+let items = ['item1', 10, true, {name:"martynas"}, 'Penktadienis']; //array
+let addItem = items.push('Super item');
+items.pop(); //pashalinti itema virs jo
+
+console.log(items.length);
+
+for(let i=0; i<items.length;i++){
+    console.log(i+" -"+items[i])
+     // break;
+    // continue;
+}
+
+
+*/
+/*let name = prompt('Iveskite svecio varda');
+
+console.log("ivestas vardas: "+name);
+if (console.log("ivestas vardas")){
+    console.log('Iveskite varda')
+}
+*/
+let guests = []
+let countGuests = prompt ("Kiek sveciu: ");
+for (let i =0 )
